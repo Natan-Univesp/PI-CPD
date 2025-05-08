69feb29bf932a53974134bec1fbac98389ba9a5f
meli-tortelli <melissa.tortelli@gmail.com>
2025-05-08 01:38:04 -0300

    Implemento da parte feito pelo Paulo.

--- client/src/components/Cards/CardMarca/CardMarca.jsx
+++ client/src/components/Cards/CardMarca/CardMarca.jsx
@@ -1,12 +1,69 @@
-//Assim eu faço a exportação do meu arquivo CSS
-//OBS: todos os "componentes" devem ter o seu css único seguindo a estrutura de "nome.module.css"
-// a aplicação da estilização com classes estão indicadas no exemplo abaixo (é um pouco diferente do HTML e CSS padrão)
-import styles from "./CardMarca.module.css";
+import styles from './CardMarca.module.css';
 
 export function CardMarca() {
-    return(
-        <>
-            <p className={styles.example}>Aqui será o local onde poderá ser adicionado todo o html de estruturação do Card de Ação</p>
-        </>
-    )
+  return (
+    <section>
+      <div className={`${styles.container}`}>
+        <div className={`${styles.card}`}>
+          <button className={`${styles.btnEditarMarca}`}>EDITAR</button>
+          <img src='../assets/img/hp-logo.svg' alt='Logo da HP' />
+          <h2>HP</h2>
+          <p>
+            Suprimentos Cadastrados:{' '}
+            <span className={`${styles.suprimentos}`}>0</span>
+          </p>
+        </div>
+
+        <div className={`${styles.card}`}>
+          <button className={`${styles.btnEditarMarca}`}>EDITAR</button>
+          <img src='assets/img/brother-logo.svg' alt='Logo da Brother' />
+          <h2>BROTHER</h2>
+          <p>
+            Suprimentos Cadastrados:{' '}
+            <span className={`${styles.suprimentos}`}>0</span>
+          </p>
+        </div>
+
+        <div className={`${styles.card}`}>
+          <button className={`${styles.btnEditarMarca}`}>EDITAR</button>
+          <img src='assets/img/canon-logo.svg' alt='Logo da Canon' />
+          <h2>CANON</h2>
+          <p>
+            Suprimentos Cadastrados:{' '}
+            <span className={`${styles.suprimentos}`}>0</span>
+          </p>
+        </div>
+
+        <div className={`${styles.card}`}>
+          <button className={`${styles.btnEditarMarca}`}>EDITAR</button>
+          <img src='assets/img/epson-logo.svg' alt='Logo da Epson' />
+          <h2>EPSON</h2>
+          <p>
+            Suprimentos Cadastrados:{' '}
+            <span className={`${styles.suprimentos}`}>0</span>
+          </p>
+        </div>
+
+        <div className={`${styles.card}`}>
+          <button className={`${styles.btnEditarMarca}`}>EDITAR</button>
+          <img src='assets/img/lexmark-logo.svg' alt='Logo da Lexmark' />
+          <h2>LEXMARK</h2>
+          <p>
+            Suprimentos Cadastrados:{' '}
+            <span className={`${styles.suprimentos}`}>0</span>
+          </p>
+        </div>
+
+        <div className={`${styles.card}`}>
+          <button className={`${styles.btnEditarMarca}`}>EDITAR</button>
+          <img src='./assets/samsung-logo.svg' alt='Logo da Samsung' />
+          <h2>SAMSUNG</h2>
+          <p>
+            Suprimentos Cadastrados:{' '}
+            <span className={`${styles.suprimentos}`}>0</span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
--- client/src/pages/Marca/Marca.jsx
+++ client/src/pages/Marca/Marca.jsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom";
-import MainLayout from "../../components/layout/MainLayout";
+import { Outlet } from 'react-router-dom';
+import MainLayout from '../../components/layout/MainLayout';
+import { CardMarca } from '../../components/Cards/CardMarca/CardMarca';
 
 export default function Marca() {
-    return(
-        <>
-            <MainLayout title={"Marca de Suprimentos"}>
-                <Outlet/>
-            </MainLayout>
-        </>
-    )
+  return (
+    <>
+      <MainLayout title={'Marca de Suprimentos'}>
+        <Outlet />
+        <CardMarca />
+      </MainLayout>
+    </>
+  );
 }
